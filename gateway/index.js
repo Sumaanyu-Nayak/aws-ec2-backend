41f0a3a86540750354c929ac615b2e5638005610
Sumaanyu-Nayak <nayaksumaanyu@gmail.com>
2025-06-01 15:20:43 +0530

    new gatway

--- gateway/index.js
+++ gateway/index.js
@@ -14,6 +14,17 @@ app.use("/products", createProxyMiddleware({
   changeOrigin: true
 }));
 
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the API Gateway!" });
+});
+app.get("/health", (req, res) => {
+  res.json({ status: "API Gateway is running" });
+});
+app.get("/all", (req, res) => {
+  res.json({ message: "This is the API Gateway. Use /users or /products to access services." });
+});
+
 app.listen(3000, () => {
   console.log("API Gateway running on http://localhost:3000");
 });
