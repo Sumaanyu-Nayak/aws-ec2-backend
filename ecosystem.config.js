62e320a762f36201da2385f49c2c1603354230e7
Sumaanyu-Nayak <nayaksumaanyu@gmail.com>
2025-06-02 10:03:58 +0530

    pm2 production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,31 @@
 module.exports = {
   apps: [
     {
-      name: "gateway",
-      script: "gateway/index.js"
+      name: 'gateway',
+      script: './gateway/index.js',
+      watch: true,
+      env: {
+        PORT: 3000,
+        NODE_ENV: 'production',
+      },
     },
     {
-      name: "users",
-      script: "services/users/index.js"
+      name: 'users-service',
+      script: './services/users/index.js',
+      watch: true,
+      env: {
+        PORT: 4001,
+        NODE_ENV: 'production',
+      },
     },
     {
-      name: "products",
-      script: "services/products/index.js"
-    }
-  ]
+      name: 'products-service',
+      script: './services/products/index.js',
+      watch: true,
+      env: {
+        PORT: 4002,
+        NODE_ENV: 'production',
+      },
+    },
+  ],
 };
